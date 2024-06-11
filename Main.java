45d65b1dc1ddd96e2d012b29419ab03e26ac5b94
Richard Nathan Domingos <richard.nathan@philips.com>
2024-06-11 09:40:24 -0300

    Fatorial

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public int fatorial(int numero){
+        if(numero == 0 || numero == 1){
+            return 1;
+        }
+
+        return numero * (fatorial(numero - 1));
+    }
+
+}
